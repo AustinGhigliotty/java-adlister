f8bb98b8e7eeff7518a4f6707dbe07fc5e213ec2
Austin Ghigliotty <austin.ghigliotty@gmail.com>
2019-08-21 14:47:38 -0500

    finished experimenting

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,21 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+
+        String name = req.getParameter("name");
+        if (name == null) name = "World";
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.printf("<h1>Hello %s!</h1>", name);
     }
 }
---
+++ src/main/webapp/testing.jsp
@@ -0,0 +1,23 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: yolarr
+  Date: 2019-08-21
+  Time: 11:26
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+    <style><%@ include file="resources/css/style.css" %></style>
+</head>
+<body>
+<h1 class="green">Green</h1>
+<h1 class="blue">Blue</h1>
+<h2 class="red">Red</h2>
+
+<h4 class="random">Your number is: </h4>
+
+    <script><%@ include file="resources/js/randomNumber.js" %></script>
+</body>
+</html>
